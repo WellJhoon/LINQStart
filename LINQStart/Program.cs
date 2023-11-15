8e8e114fcbf8155b42cb59022d0893782ac16ef1
JhonMedina <jhon-43769@outlook.com>
2023-11-15 04:20:30 -0400

    End

--- LINQStart/Program.cs
+++ LINQStart/Program.cs
@@ -8,17 +8,87 @@ var Romos = new List<Romo>()
     new Romo(){Name = "Kleren", Brand = "Patio Doña juana", Alchol = 95.5m}
 };
 
-//Colecciones anonimas con LINQ
+var Brands = new List<Brand>()
+{
+    new Brand(){Name = "Erdiger", Country = "Alemania"},
+    new Brand(){Name = "Modelo", Country = "Mexico"},
+    new Brand(){Name = "Presidente", Country = "RD"},
+    new Brand(){Name = "NoTiene", Country = "PR"},
+    new Brand(){Name = "Republica", Country = "RD"}
+};
+
+//Select LINQ
 var Romos2 = from b in Romos
              select new { b.Name, b.Brand };
 
-foreach (var b in Romos2)
+
+
+// Where Query Expression
+var Romos3 = from b in Romos
+             where b.Brand == "Brugal" || b.Alchol >= 10
+             select new { b.Name, b.Brand };
+
+
+//Metodos
+var Romos4 = Romos.Where(b => b.Brand == "Brugal" || b.Alchol >= 10)
+                    .Select(b => new
+                    { b.Name, b.Brand });
+
+
+
+//ORDER BY
+var Romos5 = from b in Romos
+             where b.Brand == "Brugal" || b.Alchol >= 10
+             orderby b.Name //desending == Sirve para que esten ordenados desde atras hacia adelante z > a
+             select new { b.Name, b.Brand };
+
+//GROUP BY
+var Romos6 = from b in Romos
+             group b by b.Brand into groupRomos
+             select new
+             {
+                 brand = groupRomos.Key,
+                 count = groupRomos.Count(),
+             };
+
+
+//Join
+var Romos7 = from b in Romos
+             join bra in Brands on b.Brand equals bra.Name
+             select new
+             {
+                 Name = b.Name,
+                 Brand = b.Brand,
+                 Country = bra.Country
+             };
+
+
+
+
+foreach (var b in Romos7)
 {
-    Console.WriteLine(b.Brand);
+    Console.WriteLine(b.Name + " "+ b.Brand + " " + b.Country);
 }
+
+
+/*
+foreach (var b in Romos5)
+{
+    Console.WriteLine(b.Name);
+}
+*/
+
+
 public class Romo
 {
     public string? Name { get; set; }
     public string? Brand { get; set; }
     public decimal Alchol { get; set; }
+}
+
+
+public class Brand
+{
+    public string? Name { get; set; }
+    public string? Country { get; set; }
 }
